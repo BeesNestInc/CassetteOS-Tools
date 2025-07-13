28d5dc3ab9bec8c4449131bff82442dd00f5084e
mayumigit <mayumi@beesnest-inc.com>
2025-07-13 06:39:18 +0000

    chore: version up v0.0.5

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#           CassetteOS Uninstaller Script v0.0.4#
+#           CassetteOS Uninstaller Script v0.0.5#
 #   GitHub: https://github.com/BeesNestInc/CassetteOS
 #   Requires: bash, mv, rm, tr, grep, sed
 #
